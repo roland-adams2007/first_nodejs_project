f68cfddb59d8e35fd3b9f27cad4115ed7775c123
Roland Adams <adamsrolly7@gmail.com>
2025-10-02 02:32:07 +0100

    chnaged the db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,21 +4,22 @@ const dotenv = require('dotenv').config();
 let connection;
 
 async function connectDB() {
-    if (!connection) {
-        try {
-            connection = await mysql.createConnection({
-                host: process.env.DB_HOST,
-                port: process.env.DB_PORT,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASS,
-                database: process.env.DB_NAME
-            });
-        } catch (err) {
-            console.error('❌ Error connecting to MySQL: ', err);
-            process.exit(1);
-        }
+  if (!connection) {
+    try {
+      connection = await mysql.createConnection({
+        host: process.env.MYSQLHOST || process.env.DB_HOST,
+        port: process.env.MYSQLPORT || process.env.DB_PORT,
+        user: process.env.MYSQLUSER || process.env.DB_USER,
+        password: process.env.MYSQLPASSWORD || process.env.DB_PASS,
+        database: process.env.MYSQLDATABASE || process.env.DB_NAME
+      });
+      console.log('✅ Connected to MySQL!');
+    } catch (err) {
+      console.error('❌ Error connecting to MySQL: ', err);
+      process.exit(1);
     }
-    return connection;
+  }
+  return connection;
 }
 
 module.exports = connectDB;
